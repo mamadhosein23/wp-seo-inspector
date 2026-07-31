0728ed71597e7a27b70a69ede35e85ba16cc3664
Mohamad Hosein Gholami <23mhghm23@gmail.com>
2026-07-31 19:54:53 +0330

    Create utils.py

---
+++ app/utils.py
@@ -0,0 +1,22 @@
+from urllib.parse import urlparse
+import tldextract
+
+
+def normalize_domain(url: str) -> str:
+    parsed = urlparse(url)
+    extracted = tldextract.extract(parsed.netloc)
+    return ".".join(part for part in [extracted.domain, extracted.suffix] if part)
+
+
+def is_internal_link(base_domain: str, link_url: str) -> bool:
+    if not link_url:
+        return False
+
+    parsed = urlparse(link_url)
+
+    # لینک نسبی
+    if not parsed.netloc:
+        return True
+
+    link_domain = normalize_domain(link_url)
+    return link_domain == base_domain
